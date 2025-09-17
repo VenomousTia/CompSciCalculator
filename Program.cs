dd8792322cfaab05617d6e3c377ef06dfb45e9a9
Tia Christensen <Tia@lavechristensen.dk>
2025-09-17 10:10:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CompSciCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Using https://learn.microsoft.com/en-us/visualstudio/get-started/csharp/tutorial-console?view=vs-2022 as a guide
+            //Numbers to input, initialized to zero
+                double num1 = 0;
+                double num2 = 0;
+
+            //Title Display
+                Console.WriteLine("Console Calculator in C#\r");
+                Console.WriteLine("------------------------\n");
+
+            //num1 input
+                Console.WriteLine("Input your first number. Press Enter to continue");
+                num1 = Convert.ToDouble(Console.ReadLine());
+            //num2 input
+                Console.WriteLine("Input your second number. Press Enter to continue");
+                num2 = Convert.ToDouble(Console.ReadLine());
+
+            //Addition, Subtraction, Multiplication, Division
+                Console.WriteLine($"Your numbers added together: {num1} + {num2} = " + (num1 + num2));
+                Console.WriteLine($"Your numbers subtracted: {num1} - {num2} = " + (num1 - num2));
+                Console.WriteLine($"Your numbers multiplied: {num1} * {num2} = " + (num1 * num2));
+                Console.WriteLine($"Your numbers divided: {num1} / {num2} = " + (num1 / num2));
+
+            //End Program
+                Console.Write("Press any key to close the Calculator...");
+                Console.ReadKey();
+        }
+    }
+}
